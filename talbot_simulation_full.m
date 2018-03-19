01598c598856d0bd78d291b06afc312e64afc2c4
grodgers <grodgers@github.com>
2018-03-19 18:58:14 +0100

    Adding polychromatic wavefunction and pattern from materials and thickness

--- talbot_simulation_full.m
+++ talbot_simulation_full.m
@@ -7,50 +7,70 @@
 % - Phase stepping over pattern (done)
 
 %% Geometry and setup parameters:
-lambda = lambda_from_E(20); % [m] (use lambda_from_E(20) to input E in keV)
-I_pattern = [1 1];
-p_pattern = [pi 0];
+
+% g1 details
 p1 = 7e-06; % [m] period of the above mentioned pattern
-% dc_g1 = 0.5; % duty cycle of first grating 
-x_pixels = 2000; % pixels per period for simulation
-repg1 = 20; % repeat g1 so that large z aren't smeared
+m1 = 'Si'; % First material (e.g. 'Si')
+m2 = 'Au'; % Second material (e.g. 'Au')
+t1 = 6e-06; % thickness of first material [m]
+t2 = 6e-06; % thickness of second material [m]
 z = (0.01:0.01:4)*(7e-06)^2/(4*lambda);
 
-p2 = p1/2; % [m] period of 2nd grating (p1/2 for pi shift)
+% p2 and detector details
+p2 = p1/2; % period of 2nd grating [m] (p1/2 for pi shift)
 z_detector = 150; % position of detector [index of z array]
-steps = 15;
-periods = 1;
-detector_pixsize = 10e-06;
+steps = 15; % How many phase steps?
+periods = 1; % How many periods to phase step over?
 
-% t0 = 6e-06;
-% t1 = 6e-06;
-% m0 = 'Si';
-% m1 = 'Au';
-% delta0 = find_delta(m0, lambda, 'wavelength');
-% delta1 = find_delta(m1, lambda, 'wavelength');
-% beta0 = find_beta(m0, lambda, 'wavelength');
-% beta1 = find_beta(m1, lambda, 'wavelength');
-% 
-% I_pattern = [exp(-mu0*t0) exp(-mu1*t1)];
-% p_pattern = [t0*delta0 t1*delta1];
+% Source details
+E_spectrum = (20:30)*1e+03; % Energies for above intensity spectrum [eV]
+I_spectrum = ones(size(E_spectrum)); % Intensity at energy described by E_spectrum
+% source_size = 2e-06; % FWHM of source [m]
+% d_sg1 = 0.3; % source to g1 distance [m]
 
 %% Computing and visualizing options:
+x_pixels = 2000; % pixels per period for simulation
+reptimes = 20; % repeat g1 so that large z aren't smeared
 method = 1; % see fresnel_propagator.m for info
-
 periods_to_plot = 3;
 
-%% Setting up wave fronts:
-tmp = round(x_pixels/2);
-I0 = [I_pattern(1)*ones(tmp,1)' I_pattern(2)*ones(tmp,1)'];
-I0 = repmat(I0,repg1);
-I0 = I0(1,:);
-p0 = [p_pattern(1)*ones(tmp,1)' p_pattern(2)*ones(tmp,1)'];
-p0 = repmat(p0,repg1);
-p0 = p0(1,:);
+% %% Find magnification
+% M = (d_sg1+z(z_detector))/d_sg1; % magnification factor
 
-%% Running propagator:
+%% Building initial wavefunction (no more inputs needed)
+tmp = round(x_pixels/2);
+I_pattern = zeros(2*tmp, length(E_spectrum));
+for e = 1:length(E_spectrum)
+    lambda = lambda_from_E(E_spectrum(e));
+    k = 2*pi./lambda;
+    delta0 = find_delta(m1, lambda, 'wavelength');
+    delta1 = find_delta(m2, lambda, 'wavelength');
+    beta0 = find_beta(m1, lambda, 'wavelength');
+    beta1 = find_beta(m2, lambda, 'wavelength');
+    I_pattern(:,e) = [exp(-2*k*beta0*t1)*ones(tmp,1)' exp(-2*k*beta1*t2)*ones(tmp,1)'];
+    p_pattern(:,e) = [-k*t1*delta0*ones(tmp,1)' -k*t2*delta1*ones(tmp,1)'];
+end
+
+WF0 = I_pattern.*exp(1i*p_pattern);
+% enlarge wavefront for large grating
+WF0 = repmat(WF0,reptimes);
+WF0 = WF0(1:length(E_spectrum),:);
+
+% %% Convolute wavefunction with source
+% sz_WF0 = length(WF0);
+% x = -((sz_WF0-1)/2):((sz_WF0-1)/2);
+% source = exp(-(x./source_size).^2); % create gaussian source
+
+%% Running propagator to get wavefunction at a distance z downstream:
 pixsize = p1/x_pixels;
-[I_z,~] = fresnel_propagator(I0, p0, pixsize, z, lambda, method);
+for e = 1:length(E_spectrum)
+    [I_e_z(:,e),WF_e_z(:,e)] = fresnel_propagator(WF0(:,e), pixsize, z, lambda_from_E(E_spectrum(e)), method);
+end
+
+WF_z = sum(WF_e_z,2); % Sum up wavefunction from various wavelength contributions
+I_z = abs(WF_z); % Find intensity of full wavefunction
+
+%% Plotting results
 temp = periods_to_plot*x_pixels;
 temp2 = round(size(I_z,2)/2)-round(temp/2);
 figure, imagesc(I_z(:,temp2:(temp2+temp))'), colormap gray
@@ -58,4 +78,13 @@ figure, imagesc(I_z(:,temp2:(temp2+temp))'), colormap gray
 %% Phase stepping:
 [stepping_curve,phase,ac,vis]=phase_stepping(I,pixsize,detector_pixsize,p2,steps,periods);
 figure, plot(stepping_curve), xlabel('phase step'), ylabel('intensity')
+
+
+
+
+
+
+
+
+
 
